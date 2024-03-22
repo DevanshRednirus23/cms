cd56ebeb1c2c1e327bd9b4fb11efa4f447c07143
DevanshRednirus23 <devansh.rednirus@gmail.com>
2024-03-22 10:52:04 +0530

    added cms project

---
+++ resources/views/layouts/sections/scriptsIncludes.blade.php
@@ -0,0 +1,8 @@
+<!-- laravel style -->
+<script src="{{ asset('helpers.js') }}"></script>
+
+<!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+<script src="{{ asset('config.js') }}"></script>
+
+
+
